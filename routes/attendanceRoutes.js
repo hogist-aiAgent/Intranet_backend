6a7993d2edd51dc79cd3c58144087eef6fdcbafa
Tharun <hogist.aiagent@gmail.com>
2025-06-30 16:54:37 +0530

    added the attendance , staff

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const attendanceController = require('./../controllers/AttendanceControllers');
-
+//test
 router.post('/bulk-upsert', attendanceController.bulkUpsertAttendance);
 
 router.get('/by-date/:date', attendanceController.getAttendanceByDate);
